d9751196ca28d6522425f4397595f9738cc946c8
Brandon Semilla <semibran@gmail.com>
2018-11-09 13:05:14 -0800

    update stage

--- src/maps/test.js
+++ src/maps/test.js
@@ -39,15 +39,16 @@ export default {
 	},
 	units: [
 		[ "MAGE",     "mage",    "player", false,    [  4, 13 ] ],
-		[ "WARRIOR",  "warrior", "player", false,    [  5, 16 ] ],
 		[ "KNIGHT",   "knight",  "player", false,    [  6, 14 ] ],
+		[ "WARRIOR",  "warrior", "player", false,    [  5, 16 ] ],
 		[ "ROGUE",    "rogue",   "player", false,    [  3, 15 ] ],
 		[ "SORCERER", "mage",    "enemy",  "defend", [ 15,  1 ] ],
 		[ "TROLL",    "knight",  "enemy",  "defend", [ 15,  4 ] ],
 		[ "TROLL",    "knight",  "enemy",  "defend", [ 16,  4 ] ],
 		[ "ORC",      "warrior", "enemy",  "wait",   [ 16,  6 ] ],
-		[ "GOBLIN",   "rogue",   "enemy",  "attack", [  8,  6 ] ],
 		[ "TROLL",    "knight",  "enemy",  "defend", [ 10,  8 ] ],
-		[ "TROLL",    "knight",  "enemy",  "attack", [  7, 12 ] ]
+		[ "GOBLIN",   "rogue",   "enemy",  "attack", [  9, 12 ] ],
+		[ "GOBLIN",   "rogue",   "enemy",  "attack", [  8,  6 ] ],
+		[ "ORC",      "warrior", "enemy",  "attack", [  7, 12 ] ]
 	]
 }
